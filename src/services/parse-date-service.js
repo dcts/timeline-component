107180432f486f9b62f8280d693684302ba508f4
Thomas Starzynski <thomas.starzynski@unibas.ch>
2020-04-20 15:33:16 +0200

    setup date parser service

---
+++ src/services/parse-date-service.js
@@ -0,0 +1,22 @@
+export class ParseDateService {
+  /*
+   * PARSE DATE SERVICE
+   * whenever the user inputs a date it should be automatically detected
+   * a lot of formats should be supported, for example:
+   * - 1. April 1970
+   * - 1970 (only year)
+   * - 1970-12-23
+   * - 1900 12 23
+   * - 1 jan 1970
+   */
+
+  // single purpose object, each initialization runs the parsing
+  constructor(input) {
+    this.input = input;
+    return this.run(input);
+  }
+
+  run (input) {
+    return input + "_RUN";
+  }
+}
