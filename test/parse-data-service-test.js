42245d62756e5123ef870699dfdd1c4fed4d39e6
Thomas Starzynski <thomas.starzynski@unibas.ch>
2020-04-27 17:04:04 +0200

    improve test naming

--- test/parse-data-service-test.js
+++ test/parse-data-service-test.js
@@ -107,32 +107,32 @@ test('should detect combined queries in reverse order', t => {
   t.is("1960-12-07", new ParseDateService().run("1960 dec 7"));
 });
 
-test('should detect ISO String format', t => {
+test('should detect YYYY-MM-DD ISO format (ISO format)', t => {
   t.is("2020-01-12", new ParseDateService().run("2020-01-12"));
   t.is("1950-02-21", new ParseDateService().run("1950-02-21"));
 });
 
-test('should detect ISO String format (skipped leading zeros)', t => {
+test('should detect YYYY-M(M)-D(D) format (ISO format with skipped leading zeros)', t => {
   t.is("2020-01-12", new ParseDateService().run("2020-1-12"));
   t.is("1950-02-04", new ParseDateService().run("1950-2-4"));
 });
 
-test('should detect ISO String format seperated by slash', t => {
+test('should detect YYYY/MM/DD format (ISO format seperated by slash)', t => {
   t.is("1923-12-24", new ParseDateService().run("1923/12/24"));
   t.is("2001-09-11", new ParseDateService().run("2001/09/11"));
 });
 
-test('should detect ISO String format seperated by slash (skipped leading zeros)', t => {
+test('should detect YYYY/M(M)/D(D) format (ISO format seperated by slash and skipped leading zeros)', t => {
   t.is("1844-01-23", new ParseDateService().run("1844/1/23"));
   t.is("1844-05-01", new ParseDateService().run("1844/5/1"));
 });
 
-test('should detect ISO String format seperated by whitespace', t => {
+test('should detect YYYY MM DD format (ISO format seperated by whitespace)', t => {
   t.is("1945-12-01", new ParseDateService().run("1945 12 01"));
   t.is("1930-01-02", new ParseDateService().run("1930 01 02"));
 });
 
-test('should detect ISO String format seperated by whitespace  (skipped leading zeros)', t => {
+test('should detect YYYY MM DD format (ISO format seperated by whitespace skipped leading zeros)', t => {
   t.is("1945-12-01", new ParseDateService().run("1945 12 1"));
   t.is("1930-01-02", new ParseDateService().run("1930 1 2"));
 });
