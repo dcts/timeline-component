3a0082a7eab89763ee35986d03ddfd14ebf1819f
Thomas Starzynski <thomas.starzynski@unibas.ch>
2020-05-06 04:35:57 +0200

    add convetion to start private methods with _

--- src/services/parse-date-service.js
+++ src/services/parse-date-service.js
@@ -1,14 +1,14 @@
 export class ParseDateService {
   /*
-   * PARSE DATE SERVICE
-   * whenever the user inputs a date it should be automatically detected
-   * a lot of formats should be supported, for example:
-   * - 1. April 1970
-   * - 1970 (only year)
-   * - 1970-12-23
-   * - 1900 12 23
-   * - 1 jan 1970
-   */
+  * PARSE DATE SERVICE
+  * whenever the user inputs a date it should be automatically detected
+  * a lot of formats should be supported, for example:
+  * - 1. April 1970
+  * - 1970 (only year)
+  * - 1970-12-23
+  * - 1900 12 23
+  * - 1 jan 1970
+  */
 
   // single purpose object, each initialization runs the parsing
   constructor() {
@@ -20,39 +20,39 @@ export class ParseDateService {
     this.month = "??";
     this.year = "????";
 
-    const resultIsoMatch = this.input.match(this.isoMatchRegex());
-    const resultCustomMatch = this.input.match(this.customRegex());
-    const resultWeekMatch = this.input.match(this.weekMatchRegex());
-    const resultYearAndMonthMatch = this.input.match(this.yearAndMonthRegex());
+    const resultIsoMatch = this.input.match(this._isoMatchRegex());
+    const resultCustomMatch = this.input.match(this._customRegex());
+    const resultWeekMatch = this.input.match(this._weekMatchRegex());
+    const resultYearAndMonthMatch = this.input.match(this._yearAndMonthRegex());
     if (resultIsoMatch) {
       const split = resultIsoMatch[0].split(/-|\/|\s/);
       this.year  = split[0];
-      this.month = this.setWithLeadingZero(split[1]);
-      this.day   = this.setWithLeadingZero(split[2]);
+      this.month = this._setWithLeadingZero(split[1]);
+      this.day   = this._setWithLeadingZero(split[2]);
     } else if (resultYearAndMonthMatch) {
       const split = resultYearAndMonthMatch[0].split("-");
       this.year = split[0];
-      this.month = this.setWithLeadingZero(split[1]);
+      this.month = this._setWithLeadingZero(split[1]);
       this.day = "01";
     } else if (resultCustomMatch) {
       const split = resultCustomMatch[0].split(/\.|\s|\/|-/);
-      this.day = this.setWithLeadingZero(split[0]);
-      this.month = this.setWithLeadingZero(split[1]);
+      this.day = this._setWithLeadingZero(split[0]);
+      this.month = this._setWithLeadingZero(split[1]);
       this.year = split[2];
     } else if (resultWeekMatch) {
       const split = resultWeekMatch[0].split(/\.|\s|\/|-/);
       const year = Number(split[0]);
       const week = Number(split[1].replace("W0", "").replace("W", ""));
-      return this.getDateStrOfISOWeek(year, week);
+      return this._getDateStrOfISOWeek(year, week);
     } else {
-      this.findYear();
-      this.findMonth();
-      this.findDay();
+      this._findYear();
+      this._findMonth();
+      this._findDay();
     }
-    return this.buildResult();
+    return this._buildResult();
   }
 
-  buildResult() {
+  _buildResult() {
     if (this.year != "????" && this.month === "??") {
       this.month = "01";
     }
@@ -63,49 +63,49 @@ export class ParseDateService {
   }
 
   // allowed 2012-01-31 /  2012-1-31 / 2012/01/31 / 2012/1/31 / 2012 1 31 / 2012 01 31
-  isoMatchRegex() {
+  _isoMatchRegex() {
     return /(?<=\s|^)\d{4}(-|\s|\/)([0][1-9]|[1-9]|10|11|12)(-|\s|\/)([0][1-9]|[1-2][0-9]|3[01]|[1-9])(?=\s|$|\.)/;
     //      |        | year         | 01-09 | 1-9 | 10-12   |         |01-09  |10-29     |30,31| 1-9  |
     //      |             | dash or slash                   | dash or slash                           |
     //      |preceding with space or start of string                                                  | end with space endofstr or dot
   }
 
-  customRegex() {
+  _customRegex() {
     return /\d{1,2}(\.|\s|\/|-)\d{1,2}(\.|\s|\/|-)\d{4}/;
   }
 
-  weekMatchRegex() {
+  _weekMatchRegex() {
     return /\d{4}(\.|\s|\/|-)W\d{1,2}(?=\s|$|\.)/;
   }
 
-  yearAndMonthRegex() {
+  _yearAndMonthRegex() {
     return /(?<=\s|^)\d{4}-([0][1-9]|[1-9]|10|11|12)(?=\s|$)/;
   }
 
-  findYear() {
+  _findYear() {
     let regex = /[1-9]{1}[0-9]{3}/;
     const result = this.input.match(regex)
     if (result) {
       this.year = result[0];
-      this.removeMatchFromInput(result);
+      this._removeMatchFromInput(result);
     }
   }
 
-  findMonth() {
-    const months = this.monthDictionaryValues();
+  _findMonth() {
+    const months = this._monthDictionaryValues();
     months.forEach(month => {
       let re = new RegExp(`(?<=\\s|^)(${month})(?=\\s|$|\\.)`, "i")
       const result = this.input.match(re);
       if (result) { // yes => get dict and value + return
-        this.month = this.monthDictionary()[result[0].toLowerCase()];
-        this.removeMatchFromInput(result);
+        this.month = this._monthDictionary()[result[0].toLowerCase()];
+        this._removeMatchFromInput(result);
         return this.month;
       }
     })
     return undefined;
   }
 
-  findDay() {
+  _findDay() {
     // find single numbers from 1 - 31
     let regex = /(?<=\s|^)([0][1-9]|[1-2][0-9]|3[01]|[1-9])(?=\s|$|\.|st|nd|rd|th)/;
     //           |         | 01-09 | 10-29    |30,31|1-9    | ends with whitespace, endofstr or dot.
@@ -114,11 +114,11 @@ export class ParseDateService {
     //           | https://stackoverflow.com/a/6713378/6272061
     const result = this.input.match(regex)
     if (result) {
-      this.day = this.setWithLeadingZero(result[0]);
+      this.day = this._setWithLeadingZero(result[0]);
     }
   }
 
-  setWithLeadingZero(str) {
+  _setWithLeadingZero(str) {
     if (str.length == 1) {
       return "0" + str;
     } else {
@@ -126,7 +126,7 @@ export class ParseDateService {
     }
   }
 
-  removeMatchFromInput(matchObj) {
+  _removeMatchFromInput(matchObj) {
     if (matchObj && matchObj[0] && matchObj.index) {
       let len = matchObj[0].length;
       let charArr = this.input.split('');
@@ -135,10 +135,10 @@ export class ParseDateService {
     }
   }
 
-  monthDictionaryValues() {
-    return Object.keys(this.monthDictionary());
+  _monthDictionaryValues() {
+    return Object.keys(this._monthDictionary());
   }
-  monthDictionary() {
+  _monthDictionary() {
     return {
       // german
       "jan": "01", "januar":    "01",
@@ -195,7 +195,7 @@ export class ParseDateService {
     }
   }
 
-  getDateStrOfISOWeek(y, w) {
+  _getDateStrOfISOWeek(y, w) {
     let simple = new Date(y, 0, 1 + (w - 1) * 7);
     // let simple = new Date(Date.UTC(y, 0, 2, 1 + (w - 1) * 7));
     let dow = simple.getDay();
@@ -211,10 +211,10 @@ export class ParseDateService {
     } else if (ISOweekStart.getFullYear() < y) {
       return `${y}-01-01`;
     }
-    return this.formatDate(ISOweekStart);
+    return this._formatDate(ISOweekStart);
   }
 
-  formatDate(date) { // target is ISO format => 2020-05-31
+  _formatDate(date) { // target is ISO format => 2020-05-31
     let d = new Date(date);
     let month = '' + (d.getMonth() + 1);
     let day = '' + d.getDate();
